c06ee510fa3af221503072766d3f89dda1493360
ChuYong <jakgon@naver.com>
2023-11-21 17:51:20 +0900

    feat: add JwtAuthenticationHandler with properties

--- config/src/main/java/com/oing/config/SpringSecurityConfig.java
+++ config/src/main/java/com/oing/config/SpringSecurityConfig.java
@@ -2,6 +2,8 @@ package com.oing.config;
 
 import com.oing.config.filter.JwtAccessDeniedHandler;
 import com.oing.config.filter.JwtAuthenticationEntryPoint;
+import com.oing.config.filter.JwtAuthenticationHandler;
+import com.oing.config.properties.WebProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * no5ing-server
@@ -19,8 +22,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @Configuration
 public class SpringSecurityConfig {
+    /**
+     * JWT 엑세스 토큰을 통한 인증 처리기
+     */
+    private final JwtAuthenticationHandler authenticationHandler;
+
+    /**
+     * 인증/인가 관련 실패 처리기
+     */
     private final JwtAccessDeniedHandler accessDeniedHandler;
     private final JwtAuthenticationEntryPoint entryPoint;
+
+    private final WebProperties webProperties;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,9 +42,13 @@ public class SpringSecurityConfig {
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authorizeHttpRequests(requestsManagement ->
-                        requestsManagement.anyRequest().permitAll()
-                )
+                .authorizeHttpRequests(requestsManagement ->{
+                        for(String whitelistUrlPath : webProperties.urlWhitelists()) {
+                            requestsManagement.requestMatchers(whitelistUrlPath).permitAll();
+                        }
+                        requestsManagement.anyRequest().authenticated();
+                })
+                .addFilterBefore(authenticationHandler, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exceptionHandlerManagement ->
                         exceptionHandlerManagement
                                 .authenticationEntryPoint(entryPoint)
