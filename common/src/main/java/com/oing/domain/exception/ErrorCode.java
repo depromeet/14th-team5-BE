9984d509710fdea405b00024544418c7e877dcf4
송영민 (YeongMin Song) <jakgon@naver.com>
2023-11-22 15:04:30 +0900

    [OING-18] feat: 공통 모듈 구축하기 (#5)
    * chore: add common module
    
    * chore: add common module to workflow
    
    * feat: add common exception classes
    
    * feat: alter DomainException to non abstract class
    
    * test: add DomainException unit test
    
    * test: add ErrorResponse unit test
    
    * chore: fix workflow annotations

---
+++ common/src/main/java/com/oing/domain/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.oing.domain.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    /**
+     * Common Errors
+     */
+    UNKNOWN_SERVER_ERROR("CM0001", "Unknown Server Error"),
+    INVALID_INPUT_VALUE("CM0002", "Invalid Input Value"),
+    /**
+     * Auth Related Errors
+     */
+    AUTHENTICATION_FAILED("AU0001", "Authentication failed"),
+    AUTHORIZATION_FAILED("AU0002", "No Permission");
+
+    private final String code;
+    private final String message;
+}
---
+++ common/src/test/java/com/oing/dto/response/ErrorResponseTest.java
@@ -0,0 +1,38 @@
+package com.oing.dto.response;
+
+import com.oing.domain.exception.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ErrorResponseTest {
+    @DisplayName("ErrorResponse.of(String, String) 메서드 테스트")
+    @Test
+    void testOfMethod() {
+        //given
+        String errorCode = "AU0001";
+        String errorMessage = "Authentication failed";
+
+        //when
+        ErrorResponse errorResponse = ErrorResponse.of(errorCode, errorMessage);
+
+        //then
+        assertEquals(errorResponse.code(), errorCode);
+        assertEquals(errorResponse.message(), errorMessage);
+    }
+
+    @DisplayName("ErrorResponse.of(ErrorCode) 메서드 테스트")
+    @Test
+    void testOfMethod2() {
+        //given
+        ErrorCode errorCode = ErrorCode.AUTHENTICATION_FAILED;
+
+        //when
+        ErrorResponse errorResponse = ErrorResponse.of(errorCode);
+
+        //then
+        assertEquals(errorResponse.code(), errorCode.getCode());
+        assertEquals(errorResponse.message(), errorCode.getMessage());
+    }
+}
