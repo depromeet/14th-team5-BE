0427ca469eba229e4263906b44ee28526f88d773
송영민 (YeongMin Song) <jakgon@naver.com>
2023-11-24 09:53:08 +0900

    [OINT-19] feature: 스프링 시큐리티 설정 (#6)
    * chore: change root project name
    
    * feat: add SpringSecurityConfig
    
    * feat: add auth handler filters
    
    * feat: add JwtAuthenticationHandler with properties
    
    * feat: add WebRequestInterceptor with filters
    
    * chore: change root project name
    
    * feat: add SpringSecurityConfig
    
    * feat: add auth handler filters
    
    * feat: add JwtAuthenticationHandler with properties
    
    * feat: add WebRequestInterceptor with filters
    
    * feat: change response to ErrorResponse
    
    * feat: add WebExceptionHandler
    
    * style: change line length under max line length
    
    * style: change line length under max line length
    
    * style: match line convention

---
+++ config/src/main/java/com/oing/config/SpringSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.oing.config;
+
+import com.oing.config.filter.JwtAccessDeniedHandler;
+import com.oing.config.filter.JwtAuthenticationEntryPoint;
+import com.oing.config.filter.JwtAuthenticationHandler;
+import com.oing.config.properties.WebProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.Arrays;
+
+/**
+ * no5ing-server
+ * User: CChuYong
+ * Date: 2023/11/21
+ * Time: 4:38 PM
+ */
+@RequiredArgsConstructor
+@Configuration
+public class SpringSecurityConfig {
+    /**
+     * JWT 엑세스 토큰을 통한 인증 처리기
+     */
+    private final JwtAuthenticationHandler authenticationHandler;
+
+    /**
+     * 인증/인가 관련 실패 처리기
+     */
+    private final JwtAccessDeniedHandler accessDeniedHandler;
+    private final JwtAuthenticationEntryPoint entryPoint;
+
+    private final WebProperties webProperties;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(requestsManagement ->{
+                    RequestMatcher[] whitelistedMatchers = Arrays.stream(webProperties.urlWhitelists())
+                            .map(AntPathRequestMatcher::new)
+                            .toArray(AntPathRequestMatcher[]::new);
+
+                    requestsManagement
+                            .requestMatchers(whitelistedMatchers).permitAll()
+                            .anyRequest().authenticated();
+                })
+                .addFilterBefore(authenticationHandler, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionHandlerManagement ->
+                        exceptionHandlerManagement
+                                .authenticationEntryPoint(entryPoint)
+                                .accessDeniedHandler(accessDeniedHandler)
+                )
+                .build();
+    }
+}
