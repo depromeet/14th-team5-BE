2deb8ce4fb5a8c25619fc0f8daea382d83a21b7b
송영민 <jakgon@naver.com>
2023-11-25 18:26:59 +0900

    feat: add token related domain classes

---
+++ member/src/main/java/com/oing/domain/exception/TokenNotValidException.java
@@ -0,0 +1,13 @@
+package com.oing.domain.exception;
+
+/**
+ * no5ing-server
+ * User: CChuYong
+ * Date: 2023/11/25
+ * Time: 6:24 PM
+ */
+public class TokenNotValidException extends DomainException {
+    public TokenNotValidException() {
+        super(ErrorCode.AUTHENTICATION_FAILED);
+    }
+}
