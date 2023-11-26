c8c6702726a23207e4f939bc7dfc9e43780e96f0
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2023-11-26 17:36:20 +0900

    [OING-24] chore: Swagger 환경 세팅 (#10)
    * feat: add SwaggerConfig
    
    * feat: add SwaggerConfig environment variable
    
    * feat: exclude swagger-related requests from Interceptor path patterns
    
    * style: change swagger title

---
+++ config/src/main/java/com/oing/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.oing.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@OpenAPIDefinition(
+        servers = @Server(url = "/", description = "Default Server URL"),
+        info = @Info(
+                title = "no5ing 백엔드 API 명세",
+                description = "springdoc을 이용한 Swagger API 문서입니다.",
+                version = "1.0",
+                contact = @Contact(
+                        name = "springdoc 공식문서",
+                        url = "https://springdoc.org/"
+                )
+        )
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("JWT", bearerAuth()));
+    }
+
+    public SecurityScheme bearerAuth() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+    }
+}
--- config/src/main/java/com/oing/config/filter/WebRequestInterceptor.java
+++ config/src/main/java/com/oing/config/filter/WebRequestInterceptor.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -26,6 +27,9 @@ public class WebRequestInterceptor implements HandlerInterceptor {
     public boolean preHandle(
             HttpServletRequest request, HttpServletResponse response, Object handler
     ) throws Exception {
+        if (isPreflight(request) || isSwaggerRequest(request)) {
+            return true;
+        }
         long startTime = System.currentTimeMillis();
         request.setAttribute(START_TIME_ATTR_NAME, startTime);
         return true;
@@ -44,5 +48,14 @@ public class WebRequestInterceptor implements HandlerInterceptor {
         String forwardedIp = request.getHeader(webProperties.headerNames().proxyForwardHeader());
         String originIp = forwardedIp != null ? forwardedIp : request.getRemoteAddr();
         log.info("{} {} {} {}ms", request.getMethod(), request.getRequestURI(), originIp, executionTime);
+    }
+
+    private boolean isSwaggerRequest(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        return uri.contains("swagger") || uri.contains("api-docs") || uri.contains("webjars");
+    }
+
+    private boolean isPreflight(HttpServletRequest request) {
+        return request.getMethod().equals(HttpMethod.OPTIONS.toString());
     }
 }
