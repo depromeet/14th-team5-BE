c3558cb9ffc88424b449cd52e7f07d6c98a5d55e
ChuYong <jakgon@naver.com>
2023-11-27 13:22:59 +0900

    feat: report slack on error

--- config/src/main/java/com/oing/ServerApplication.java
+++ config/src/main/java/com/oing/ServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableJpaAuditing
+@EnableAsync
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class ServerApplication {
