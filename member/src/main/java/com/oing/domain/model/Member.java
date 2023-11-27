4e0244a4244c24eca252e466743d2ffa03aa4d7c
ChuYong <jakgon@naver.com>
2023-11-27 11:53:16 +0900

    feat: add Member and SocialMember entity

---
+++ member/src/main/java/com/oing/domain/model/Member.java
@@ -0,0 +1,25 @@
+package com.oing.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * no5ing-server
+ * User: CChuYong
+ * Date: 2023/11/27
+ * Time: 11:31 AM
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@Entity(name = "member")
+public class Member extends BaseAuditEntity {
+    @Id
+    @Column(name = "member_id", length = 26)
+    private String id;
+}
