ff5a3ab7eeb7fa213d97362edb16a175acb22aa5
ChuYong <jakgon@naver.com>
2023-11-27 11:40:36 +0900

    feat: create BaseAuditEntity

--- config/src/main/java/com/oing/ServerApplication.java
+++ config/src/main/java/com/oing/ServerApplication.java
@@ -3,7 +3,9 @@ package com.oing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class ServerApplication {
