eb3cd257a0cc7b38a44b5dcd68a1191cdefa0077
jisu <dlawotn3@naver.com>
2023-12-05 13:03:01 +0900

    feat: document PreSignedUrlResponse

--- common/src/main/java/com/oing/dto/response/PreSignedUrlResponse.java
+++ common/src/main/java/com/oing/dto/response/PreSignedUrlResponse.java
@@ -1,6 +1,10 @@
 package com.oing.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "PreSingedUrl 요청 후 응답")
 public record PreSignedUrlResponse(
+        @Schema(description = "PreSingedUrl", example = "")
         String url
 ){
 }
