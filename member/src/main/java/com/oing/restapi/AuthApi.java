db14c37bc4f616c27c61032d05a0be5cb58d4ced
ChuYong <jakgon@naver.com>
2023-12-05 11:19:30 +0900

    docs: add more description and example to api docs

--- member/src/main/java/com/oing/restapi/AuthApi.java
+++ member/src/main/java/com/oing/restapi/AuthApi.java
@@ -3,6 +3,7 @@ package com.oing.restapi;
 import com.oing.dto.request.NativeSocialLoginRequest;
 import com.oing.dto.response.AuthResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
@@ -18,9 +19,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/v1/auth")
 public interface AuthApi {
     @Operation(summary = "네이티브 소셜 로그인", description = "네이티브(apple 등) 소셜 로그인을 진행합니다.")
-    @PostMapping(value = "/social")
+    @PostMapping(value = "/social/{provider}")
     AuthResultResponse socialLogin(
-            @RequestParam("provider") String provider,
+            @PathVariable("provider") @Parameter(example = "APPLE", description = "oAuth 제공자 이름") String provider,
             @RequestBody @Valid NativeSocialLoginRequest request
     );
 }
