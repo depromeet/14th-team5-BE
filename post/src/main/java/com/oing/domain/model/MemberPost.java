8e67f9b11d277417bceb4bd51116898fb5381861
jisu <dlawotn3@naver.com>
2023-12-08 18:15:55 +0900

    feat: add MemberPost Entity

---
+++ post/src/main/java/com/oing/domain/model/MemberPost.java
@@ -0,0 +1,33 @@
+package com.oing.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity(name = "member_post")
+public class MemberPost extends BaseAuditEntity {
+    @Id
+    @Column(name = "post_id", length = 26, columnDefinition = "CHAR(26)")
+    private String id;
+
+    @Column(name = "member_id", length = 26, columnDefinition = "CHAR(26)")
+    private String memberId;
+
+    @Column(name = "post_date", nullable = false)
+    private LocalDate postDate;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "comment_cnt", nullable = false)
+    private int commentCnt;
+
+    @Column(name = "reaction_cnt", nullable = false)
+    private int reactionCnt;
+}
