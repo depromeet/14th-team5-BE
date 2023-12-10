78814601aed6293b9c3cd9160a45a962f80807bd
Kwon770 <sckwon770@gmail.com>
2023-12-11 02:30:38 +0900

    refactor: Remove @Repository annotaion

--- family/src/main/java/com/oing/repository/FamilyRepository.java
+++ family/src/main/java/com/oing/repository/FamilyRepository.java
@@ -4,6 +4,5 @@ import com.oing.domain.Family;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FamilyRepository extends JpaRepository<Family, String> {
 }
