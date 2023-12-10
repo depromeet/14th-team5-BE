8fa066ce21dad54823d45df379ee886e0cbae7c1
sckwon770 <sckwon770@gmail.com>
2023-12-11 07:18:46 +0900

    [OING-56] feat: Family 모듈 구성 (#31)

---
+++ family/src/main/java/com/oing/repository/FamilyRepository.java
@@ -0,0 +1,8 @@
+package com.oing.repository;
+
+import com.oing.domain.Family;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface FamilyRepository extends JpaRepository<Family, String> {
+}
