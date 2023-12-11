9b6dd6579d48027858ca460cc9fbaa57bca67d88
Kwon770 <sckwon770@gmail.com>
2023-12-11 16:14:52 +0900

    fix: Fix compile error at MemberController

--- member/src/main/java/com/oing/dto/response/MemberResponse.java
+++ member/src/main/java/com/oing/dto/response/MemberResponse.java
@@ -1,5 +1,7 @@
 package com.oing.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * no5ing-server
  * User: CChuYong
@@ -7,6 +9,13 @@ package com.oing.dto.response;
  * Time: 6:03 PM
  */
 public record MemberResponse(
+        @Schema(description = "구성원 ID", example = "01HGW2N7EHJVJ4CJ999RRS2E")
+        String memberId,
 
+        @Schema(description = "구성원 이름", example = "디프만")
+        String name,
+
+        @Schema(description = "구성원 프로필 이미지 주소", example = "https://asset.no5ing.kr/post/01HGW2N7EHJVJ4CJ999RRS2E97")
+        String imageUrl
 ) {
 }
