6a6c17f3f1c9b1d578aa0dc85906dc7ef8b960de
jisu <dlawotn3@naver.com>
2023-12-14 23:25:16 +0900

    feat: add updateMember mock api

--- member/src/main/java/com/oing/controller/MemberController.java
+++ member/src/main/java/com/oing/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.oing.controller;
 
+import com.oing.domain.exception.DomainException;
+import com.oing.domain.exception.ErrorCode;
 import com.oing.dto.request.UpdateMemberRequest;
 import com.oing.dto.response.FamilyMemberProfileResponse;
 import com.oing.dto.response.MemberResponse;
@@ -48,11 +50,18 @@ public class MemberController implements MemberApi {
 
     @Override
     public MemberResponse updateMember(String memberId, UpdateMemberRequest request) {
-        //TODO: 프로필 이미지 수정 로직 추가
-        return new MemberResponse(
-                memberId,
-                request.name(),
-                request.profileImageUrl()
-        );
+        String memberIdBase = "01HGW2N7EHJVJ4CJ999RRS2E";
+        memberId = "01HGW2N7EHJVJ4CJ999RRS2E";
+
+        //TODO: 로그인한 사용자의 ID와 정보수정 대상 사용자의 ID가 같은지 확인
+        if (memberIdBase.equals(memberId)) {
+            //TODO: 프로필 이미지 및 닉네임 수정 로직 추가
+            return new MemberResponse(
+                    memberId,
+                    request.name(),
+                    request.profileImageUrl()
+            );
+        }
+        throw new DomainException(ErrorCode.AUTHORIZATION_FAILED);
     }
 }
