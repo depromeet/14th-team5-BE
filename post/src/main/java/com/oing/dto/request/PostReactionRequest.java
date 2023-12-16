68b537186033633583beb092eba78aa19fdbdebd
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2023-12-16 12:38:54 +0900

    [OING-68] feat: Post Reaction API 모킹 (#40)
    * feat: add PostReaction mock API
    
    * feat: change PostReaction DTO name
    
    * feat: change reactToPost API name

---
+++ post/src/main/java/com/oing/dto/request/PostReactionRequest.java
@@ -0,0 +1,19 @@
+package com.oing.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * no5ing-server
+ * User: CChuYong
+ * Date: 2023/12/11
+ * Time: 11:30 PM
+ */
+@Schema(description = "피드 게시물 반응 생성 및 삭제 요청")
+public record PostReactionRequest(
+        @NotBlank
+        @Schema(description = "이모지", example = "smile",
+                allowableValues = {"smile", "heart", "clap", "thumbs-up", "laugh"})
+        String content
+) {
+}
