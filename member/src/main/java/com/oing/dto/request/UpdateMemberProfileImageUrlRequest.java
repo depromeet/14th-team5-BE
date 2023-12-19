ffdee7fa12346f3329849441fee7f9f7ceda0d78
jisu <dlawotn3@naver.com>
2023-12-20 05:03:22 +0900

    style: update imageUrl RequestDTO example

--- member/src/main/java/com/oing/dto/request/UpdateMemberProfileImageUrlRequest.java
+++ member/src/main/java/com/oing/dto/request/UpdateMemberProfileImageUrlRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "회원 프로필 이미지 수정 요청")
 public record UpdateMemberProfileImageUrlRequest(
-        @Schema(description = "회원 프로필 사진 주소", example = "https://asset.no5ing.kr/member/01HGW2N7EHJVJ4CJ999RRS2E97")
+        @Schema(description = "회원 프로필 사진 주소", example = "images/profile/01HJ1SEEEFDSA5ZZVRW.jpg")
         String profileImageUrl
 ) {
 }
