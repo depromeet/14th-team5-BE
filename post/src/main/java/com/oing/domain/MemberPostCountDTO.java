f8dcb1c5e52c64f636de09163394756e3c44f561
Kwon770 <sckwon770@gmail.com>
2023-12-24 03:33:09 +0900

    fix: Add NoArgsContructor at MemberPostCountDTO to fix QueryDSL projections QBean runtime error

--- post/src/main/java/com/oing/domain/MemberPostCountDTO.java
+++ post/src/main/java/com/oing/domain/MemberPostCountDTO.java
@@ -2,10 +2,12 @@ package com.oing.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MemberPostCountDTO {
 
