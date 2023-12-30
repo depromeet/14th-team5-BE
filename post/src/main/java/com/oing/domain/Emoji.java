023b96ffc600414c2e6761eed357b2135a31cb08
ChuYong <jakgon@naver.com>
2023-12-30 14:23:35 +0900

    feat: change emoji type name

--- post/src/main/java/com/oing/domain/Emoji.java
+++ post/src/main/java/com/oing/domain/Emoji.java
@@ -9,18 +9,21 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum Emoji {
 
-    HEART("heart"), SLIGHTLY_SMILING_FACE("slightly_smiling_face"), SHINING_FACE("shining_face"),
-    SMILING_FACE("smiling_face"), SMILE("smile");
+    EMOJI_1("emoji_1"),
+    EMOJI_2("emoji_2"),
+    EMOJI_3("emoji_3"),
+    EMOJI_4("emoji_4"),
+    EMOJI_5("emoji_5");
 
     private final String typeKey;
 
     public static Emoji fromString(String typeKey) {
         return switch (typeKey.toUpperCase()) {
-            case "HEART" -> HEART;
-            case "SLIGHTLY_SMILING_FACE" -> SLIGHTLY_SMILING_FACE;
-            case "SHINING_FACE" -> SHINING_FACE;
-            case "SMILING_FACE" -> SMILING_FACE;
-            case "SMILE" -> SMILE;
+            case "EMOJI_1" -> EMOJI_1;
+            case "EMOJI_2" -> EMOJI_2;
+            case "EMOJI_3" -> EMOJI_3;
+            case "EMOJI_4" -> EMOJI_4;
+            case "EMOJI_5" -> EMOJI_5;
             default -> throw new DomainException(ErrorCode.INVALID_INPUT_VALUE);
         };
     }
