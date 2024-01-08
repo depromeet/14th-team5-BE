b41736470dce6fbb640abc48fe023ae6d6c757cb
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2024-01-09 00:00:18 +0900

    [OING-130] feat: postDate 컬럼 삭제 및 가족이 없는 멤버에 대해 가족 프로필 조회 시, 예외 반환 로직 추가 (#83)
    * feat: delete postDate column in post
    
    * style: change method name
    
    * feat: add delete postdate column sql
    
    * fix: fix existsByMemberIdAndCreatedAt method
    
    * test: fix memberPost test
    
    * feat: add FamilyNotFoundException

--- post/src/test/java/com/oing/domain/model/MemberPostTest.java
+++ post/src/test/java/com/oing/domain/model/MemberPostTest.java
@@ -17,13 +17,12 @@ public class MemberPostTest {
         // Given
         String postId = "samplePostId";
         String memberId = "sampleMemberId";
-        LocalDate postDate = LocalDate.of(2023, 7, 8);
         String imageUrl = "https://picsum.photos/200/300?random=1";
         String imageKey = "/200/300?random=1";
         String content = "밥 맛있다!";
 
         // When
-        MemberPost post = new MemberPost(postId, memberId, postDate, imageUrl, imageKey, content, 0,
+        MemberPost post = new MemberPost(postId, memberId, imageUrl, imageKey, content, 0,
                 0, null, null);
 
         // Then
