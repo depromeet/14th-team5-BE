3e86f1a4d938bf883f9b310f38b6924907c15340
jisu <dlawotn3@naver.com>
2024-01-08 23:34:50 +0900

    fix: fix existsByMemberIdAndCreatedAt method

--- post/src/main/java/com/oing/repository/MemberPostRepository.java
+++ post/src/main/java/com/oing/repository/MemberPostRepository.java
@@ -3,8 +3,5 @@ package com.oing.repository;
 import com.oing.domain.MemberPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
-
 public interface MemberPostRepository extends JpaRepository<MemberPost, String>, MemberPostRepositoryCustom {
-    boolean existsByMemberIdAndCreatedAt(String memberId, LocalDate postDate);
 }
