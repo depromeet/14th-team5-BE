882c9bb155a5ad27c354429e347910e5f217aebd
jisu <dlawotn3@naver.com>
2024-01-09 17:25:24 +0900

    test: add MemberControllerTest for get Member Profile info

--- member/src/test/java/com/oing/controller/MemberControllerTest.java
+++ member/src/test/java/com/oing/controller/MemberControllerTest.java
@@ -3,6 +3,9 @@ package com.oing.controller;
 import com.oing.domain.Member;
 import com.oing.dto.request.UpdateMemberNameRequest;
 import com.oing.dto.request.UpdateMemberProfileImageUrlRequest;
+import com.oing.dto.response.FamilyMemberProfileResponse;
+import com.oing.dto.response.MemberResponse;
+import com.oing.dto.response.PaginationResponse;
 import com.oing.service.MemberService;
 import com.oing.util.AuthenticationHolder;
 import com.oing.util.PreSignedUrlGenerator;
@@ -13,13 +16,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.security.InvalidParameterException;
 import java.time.LocalDate;
+import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -38,12 +43,55 @@ public class MemberControllerTest {
     private PreSignedUrlGenerator preSignedUrlGenerator;
 
     private Member TEST_MEMBER1;
+    private Member TEST_MEMBER2;
 
     @BeforeEach
     void setUp() {
-        when(authenticationHolder.getUserId()).thenReturn("1");
         TEST_MEMBER1 = new Member("1", "1",
                 LocalDate.of(2000, 7, 8), "testMember1", null, null);
+        TEST_MEMBER2 = new Member("2", "1",
+                LocalDate.of(2003, 7, 26), "testMember2", null, null);
+    }
+
+    @Test
+    void 멤버_프로필_조회_테스트() {
+        // given
+        Member member = spy(TEST_MEMBER1);
+        when(memberService.findMemberById(any())).thenReturn(member);
+
+        // when
+        MemberResponse response = memberController.getMember(TEST_MEMBER1.getId());
+
+        // then
+        assertEquals(TEST_MEMBER1.getId(), response.memberId());
+        assertEquals(TEST_MEMBER1.getName(), response.name());
+        assertEquals(TEST_MEMBER1.getProfileImgUrl(), response.imageUrl());
+        assertEquals(TEST_MEMBER1.getFamilyId(), response.familyId());
+        assertEquals(TEST_MEMBER1.getDayOfBirth(), response.dayOfBirth());
+    }
+
+    @Test
+    void 가족_멤버_프로필_조회_테스트() {
+        // given
+        Member member1 = spy(TEST_MEMBER1);
+        Member member2 = spy(TEST_MEMBER2);
+        String familyId = "1";
+        when(authenticationHolder.getUserId()).thenReturn("1");
+        when(memberService.findFamilyIdByMemberId(anyString())).thenReturn(familyId);
+        Page<FamilyMemberProfileResponse> profilePage = new PageImpl<>(Arrays.asList(
+                new FamilyMemberProfileResponse(member1.getId(), member1.getName(), member1.getProfileImgUrl()),
+                new FamilyMemberProfileResponse(member2.getId(), member2.getName(), member2.getProfileImgUrl())
+        ));
+        when(memberService.findFamilyMembersProfilesByFamilyId(familyId, 1, 5))
+                .thenReturn(profilePage);
+
+        // when
+        PaginationResponse<FamilyMemberProfileResponse> response = memberController.
+                getFamilyMembersProfiles(1, 5);
+
+        // then
+        assertFalse(response.hasNext());
+        assertEquals(2, response.results().size());
     }
 
     @Test
@@ -52,6 +100,7 @@ public class MemberControllerTest {
         String newName = "newName";
         Member member = spy(TEST_MEMBER1);
         when(memberService.findMemberById(any())).thenReturn(member);
+        when(authenticationHolder.getUserId()).thenReturn("1");
 
         // when
         UpdateMemberNameRequest request = new UpdateMemberNameRequest(newName);
@@ -68,6 +117,7 @@ public class MemberControllerTest {
         String newName = "wrong-length-name";
         Member member = spy(TEST_MEMBER1);
         when(memberService.findMemberById(any())).thenReturn(member);
+        when(authenticationHolder.getUserId()).thenReturn("1");
 
         // when
         UpdateMemberNameRequest request = new UpdateMemberNameRequest(newName);
@@ -82,7 +132,8 @@ public class MemberControllerTest {
         String newProfileImage = "http://test.com/profile.jpg";
         Member member = spy(TEST_MEMBER1);
         when(memberService.findMemberById(any())).thenReturn(member);
-        when(preSignedUrlGenerator.extractImageKey(any())).thenReturn("/test.jpg");
+        when(authenticationHolder.getUserId()).thenReturn("1");
+        when(preSignedUrlGenerator.extractImageKey(any())).thenReturn("/profile.jpg");
 
         // when
         UpdateMemberProfileImageUrlRequest request = new UpdateMemberProfileImageUrlRequest(newProfileImage);
