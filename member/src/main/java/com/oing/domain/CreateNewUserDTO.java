6e118c5988e2076f04ad86ae25e0c2577185a7d9
jisu <dlawotn3@naver.com>
2024-01-10 17:52:14 +0900

    feat: add @NotBlank to CreateNewUserDto

--- member/src/main/java/com/oing/domain/CreateNewUserDTO.java
+++ member/src/main/java/com/oing/domain/CreateNewUserDTO.java
@@ -1,5 +1,6 @@
 package com.oing.domain;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 public record CreateNewUserDTO(
         SocialLoginProvider provider,
         String identifier,
+        @NotBlank
         @Size(min = 1, max = 9)
         String memberName,
         LocalDate dayOfBirth,
