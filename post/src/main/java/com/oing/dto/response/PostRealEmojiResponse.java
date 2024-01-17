7e9b608682ef7d1a35eda0bb270a6f7186833471
jisu <dlawotn3@naver.com>
2024-01-17 16:51:20 +0900

    feat: add getPostRealEmojis Api

--- post/src/main/java/com/oing/dto/response/PostRealEmojiResponse.java
+++ post/src/main/java/com/oing/dto/response/PostRealEmojiResponse.java
@@ -1,11 +1,13 @@
 package com.oing.dto.response;
 
-import com.oing.domain.MemberPostReaction;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "피드 게시물 응답")
+@Schema(description = "피드 게시물 리얼 이모지 응답")
 public record PostRealEmojiResponse(
         @Schema(description = "피드 게시물 리얼 이모지 ID", example = "01HGW2N7EHJVJ4CJ999RRS2E97")
+        String postRealEmojiId,
+
+        @Schema(description = "리얼 이모지 ID", example = "01HGW2N7EHJVJ4CJ999RRS2E97")
         String realEmojiId,
 
         @Schema(description = "피드 게시물 ID", example = "01HGW2N7EHJVJ4CJ999RRS2E97")
