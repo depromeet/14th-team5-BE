81eeef9908f6b6e21d40ba55134abba50e7e78db
jisu <dlawotn3@naver.com>
2024-01-19 03:14:18 +0900

    test: add EmbeddedRedisConfig

---
+++ gateway/src/test/java/com/oing/support/EmbeddedRedisConfig.java
@@ -0,0 +1,36 @@
+package com.oing.support;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+@Configuration
+@Profile("test")
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() {
+        try {
+            redisServer = RedisServer.builder()
+                    .port(port)
+                    .setting("maxmemory 256M")
+                    .build();
+            redisServer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        redisServer.stop();
+    }
+}
