45eeb20ae2e1151dfa9ccc6eda1cb1239152f620
Kwon770 <sckwon770@gmail.com>
2024-01-25 00:32:14 +0900

    feat: Change getBanner api mocking according to policy changes of banner

--- gateway/src/main/java/com/oing/dto/response/BannerResponse.java
+++ gateway/src/main/java/com/oing/dto/response/BannerResponse.java
@@ -1,5 +1,6 @@
 package com.oing.dto.response;
 
+import com.oing.domain.BannerImageType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "배너 응답")
@@ -8,6 +9,12 @@ public record BannerResponse(
         Integer familyTopPercentage,
 
         @Schema(description = "가족 구성원 모두가 업로드한 날의 수", example = "3")
-        Integer allFamilyMembersUploadedDays
+        Integer allFamilyMembersUploadedDays,
+
+        @Schema(description = "가족 활성도 레벨 (1 ~ 4)", example = "1")
+        Integer familyLevel,
+
+        @Schema(description = "배너 이미지 타입", example = "SKULL_FLAG")
+        BannerImageType bannerImageType
 ) {
 }
