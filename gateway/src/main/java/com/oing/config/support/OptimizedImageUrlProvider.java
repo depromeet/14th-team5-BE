2a4370c5c20657cc892a0ffa3eabb870bfec6ade
sckwon770 <sckwon770@gmail.com>
2024-01-31 22:16:42 +0900

    [OING-184] chore: MicroMeter Prometheus 구현체 추가 (#136)
    * chore: Add micrometer prometheus and configs
    
    * chore: Remove metrics endpoints from application.yaml for actuaor
    
    * fix: Fix broken CalendarApiTest by making optimizing query public

--- gateway/src/main/java/com/oing/config/support/OptimizedImageUrlProvider.java
+++ gateway/src/main/java/com/oing/config/support/OptimizedImageUrlProvider.java
@@ -17,8 +17,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class OptimizedImageUrlProvider implements OptimizedImageUrlGenerator {
 
-    private static final String THUMBNAIL_OPTIMIZER_QUERY_STRING = "type=f&w=96&h=96&quality=70&autorotate=false&faceopt=false";
-    private static final String KB_IMAGE_OPTIMIZER_QUERY_STRING = "?type=f&w=480&h=480&faceopt=false&quality=50&autorotate=false";
+    public static final String THUMBNAIL_OPTIMIZER_QUERY_STRING = "type=f&w=96&h=96&quality=70&autorotate=false&faceopt=false";
+    public static final String KB_IMAGE_OPTIMIZER_QUERY_STRING = "?type=f&w=480&h=480&faceopt=false&quality=50&autorotate=false";
 
     @Value("${cloud.ncp.image-optimizer-cdn}")
     private String imageOptimizerCdnUrl;
