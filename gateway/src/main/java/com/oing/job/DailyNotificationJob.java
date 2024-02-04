eb247a4f32ecaa8a80bd0ec7308d1bc50085aa53
송영민 <jakgon@naver.com>
2024-02-04 23:11:58 +0900

    fix: fix known error codes

--- gateway/src/main/java/com/oing/job/DailyNotificationJob.java
+++ gateway/src/main/java/com/oing/job/DailyNotificationJob.java
@@ -6,6 +6,7 @@ import com.google.firebase.messaging.Aps;
 import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
 import com.oing.domain.BulkNotificationCompletedEvent;
+import com.oing.domain.ErrorReportDTO;
 import com.oing.domain.Member;
 import com.oing.service.FCMNotificationService;
 import com.oing.service.MemberDeviceService;
@@ -17,6 +18,8 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
@@ -42,69 +45,77 @@ public class DailyNotificationJob {
     public void sendDailyUploadNotification() {
         long start = System.currentTimeMillis();
         log.info("[DailyNotificationJob] 오늘 업로드 알림 전송 시작");
-        HashSet<String> targetFcmTokens = new HashSet<>();
-        List<Member> members = memberService.findAllMember();
-        for (Member member : members) {
-            targetFcmTokens.addAll(memberDeviceService.getFcmTokensByMemberId(member.getId()));
-        }
+        try {
+            HashSet<String> targetFcmTokens = new HashSet<>();
+            List<Member> members = memberService.findAllMember();
+            for (Member member : members) {
+                targetFcmTokens.addAll(memberDeviceService.getFcmTokensByMemberId(member.getId()));
+            }
 
-        Lists.partition(targetFcmTokens.stream().toList(), 500).forEach(partitionedList -> {
-            MulticastMessage multicastMessage = MulticastMessage.builder()
-                    .setNotification(
-                            buildNotification("삐삐", "지금 바로 가족에게 일상 공유를 해볼까요?")
+            Lists.partition(targetFcmTokens.stream().toList(), 500).forEach(partitionedList -> {
+                MulticastMessage multicastMessage = MulticastMessage.builder()
+                        .setNotification(
+                                buildNotification("삐삐", "지금 바로 가족에게 일상 공유를 해볼까요?")
+                        )
+                        .addAllTokens(partitionedList)
+                        .setApnsConfig(buildApnsConfig())
+                        .build();
+                fcmNotificationService.sendMulticastMessage(multicastMessage);
+            });
+            log.info("[DailyNotificationJob] 오늘 업로드 알림 전송 완료. (총 {}명, {}토큰) 소요시간 : {}ms",
+                    members.size(),
+                    targetFcmTokens.size(),
+                    System.currentTimeMillis() - start);
+
+            eventPublisher.publishEvent(
+                    new BulkNotificationCompletedEvent(
+                            "오늘 업로드 알림 전송 완료", targetFcmTokens.size(), members.size(),
+                            System.currentTimeMillis() - start
                     )
-                    .addAllTokens(partitionedList)
-                    .setApnsConfig(buildApnsConfig())
-                    .build();
-            fcmNotificationService.sendMulticastMessage(multicastMessage);
-        });
-        log.info("[DailyNotificationJob] 오늘 업로드 알림 전송 완료. (총 {}명, {}토큰) 소요시간 : {}ms",
-                members.size(),
-                targetFcmTokens.size(),
-                System.currentTimeMillis() - start);
-
-        eventPublisher.publishEvent(
-                new BulkNotificationCompletedEvent(
-                        "오늘 업로드 알림 전송 완료", targetFcmTokens.size(), members.size(),
-                        System.currentTimeMillis() - start
-                )
-        );
+            );
+        } catch(Exception exception) {
+            eventPublisher.publishEvent(new ErrorReportDTO(exception.getMessage(), getStackTraceAsString(exception)));
+        }
     }
 
     @Scheduled(cron = "0 30 23 * * *", zone = "Asia/Seoul") // 11:30 PM
     public void sendDailyRemainingNotification() {
         long start = System.currentTimeMillis();
         log.info("[DailyNotificationJob] 오늘 미 업로드 사용자 대상 알림 전송 시작");
-        LocalDate today = LocalDate.now();
-        List<Member> allMembers = memberService.findAllMember();
-        HashSet<String> targetFcmTokens = new HashSet<>();
-        HashSet<String> postedMemberIds = new HashSet<>(memberPostService.getMemberIdsPostedToday(today));
-        allMembers.stream()
-                .filter(member -> !postedMemberIds.contains(member.getId())) //오늘 업로드한 사람이 아닌 사람들은
-                .forEach(member -> targetFcmTokens.addAll(memberDeviceService.getFcmTokensByMemberId(member.getId())));
+        try {
+            LocalDate today = LocalDate.now();
+            List<Member> allMembers = memberService.findAllMember();
+            HashSet<String> targetFcmTokens = new HashSet<>();
+            HashSet<String> postedMemberIds = new HashSet<>(memberPostService.getMemberIdsPostedToday(today));
+            allMembers.stream()
+                    .filter(member -> !postedMemberIds.contains(member.getId())) //오늘 업로드한 사람이 아닌 사람들은
+                    .forEach(member -> targetFcmTokens.addAll(memberDeviceService.getFcmTokensByMemberId(member.getId())));
 
-        Lists.partition(targetFcmTokens.stream().toList(), 500).forEach(partitionedList -> {
-            MulticastMessage multicastMessage = MulticastMessage.builder()
-                    .setNotification(
-                         buildNotification("삐삐", "사진을 공유할 수 있는 시간이 얼마 남지 않았어요.")
+            Lists.partition(targetFcmTokens.stream().toList(), 500).forEach(partitionedList -> {
+                MulticastMessage multicastMessage = MulticastMessage.builder()
+                        .setNotification(
+                                buildNotification("삐삐", "사진을 공유할 수 있는 시간이 얼마 남지 않았어요.")
+                        )
+                        .addAllTokens(partitionedList)
+                        .setApnsConfig(buildApnsConfig())
+                        .build();
+                fcmNotificationService.sendMulticastMessage(multicastMessage);
+            });
+            log.info("[DailyNotificationJob] 오늘 미 업로드 사용자 대상 알림 전송 완료. (총 {}명, {}토큰) 소요시간 : {}ms",
+                    allMembers.size() - postedMemberIds.size(),
+                    targetFcmTokens.size(),
+                    System.currentTimeMillis() - start);
+
+            eventPublisher.publishEvent(
+                    new BulkNotificationCompletedEvent(
+                            "오늘 미업로드자 업로드 알림 전송 완료", targetFcmTokens.size(),
+                            allMembers.size() - postedMemberIds.size(),
+                            System.currentTimeMillis() - start
                     )
-                    .addAllTokens(partitionedList)
-                    .setApnsConfig(buildApnsConfig())
-                    .build();
-            fcmNotificationService.sendMulticastMessage(multicastMessage);
-        });
-        log.info("[DailyNotificationJob] 오늘 미 업로드 사용자 대상 알림 전송 완료. (총 {}명, {}토큰) 소요시간 : {}ms",
-                allMembers.size() - postedMemberIds.size(),
-                targetFcmTokens.size(),
-                System.currentTimeMillis() - start);
-
-        eventPublisher.publishEvent(
-                new BulkNotificationCompletedEvent(
-                        "오늘 미업로드자 업로드 알림 전송 완료", targetFcmTokens.size(),
-                        allMembers.size() - postedMemberIds.size(),
-                        System.currentTimeMillis() - start
-                )
-        );
+            );
+        } catch(Exception exception) {
+            eventPublisher.publishEvent(new ErrorReportDTO(exception.getMessage(), getStackTraceAsString(exception)));
+        }
     }
 
     private Notification buildNotification(String title, String body){
@@ -118,5 +129,11 @@ public class DailyNotificationJob {
         return ApnsConfig.builder()
                 .setAps(Aps.builder().setSound("default").build())
                 .build();
+    }
+
+    private String getStackTraceAsString(Throwable throwable) {
+        StringWriter stringWriter = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
     }
 }
