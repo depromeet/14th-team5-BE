d8c71088d0f1deb64b9a20bfedf476cb9f75b188
Kwon770 <sckwon770@gmail.com>
2024-02-10 05:35:25 +0900

    chore: Add detail explanation about API at Swagger operation description

--- gateway/src/main/java/com/oing/restapi/CalendarApi.java
+++ gateway/src/main/java/com/oing/restapi/CalendarApi.java
@@ -23,8 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/calendar")
 public interface CalendarApi {
 
-    @Operation(summary = "월별 캘린더 조회", description = "월별로 캘린더를 조회합니다. 각 날짜의 대표 게시글 정보가 조회되며, 해당 날짜에 게시글이 있는 경우만 response 에 포함됩니다.")
-    @GetMapping(params = {"type=MONTHLY"})
+    @Operation(summary = "월별 썸네일 조회", description = "월별로 캘린더의 대표 게시물 썸네일을 조회합니다. 각 날짜의 대표 게시글 정보가 조회되며, 해당 날짜에 게시글이 있는 경우만 response 에 포함됩니다.\n\n ⚠️ 더 이상 type=MONTHLY 를 사용하지 않습니다.")
+    @GetMapping("/thumbnails")
     ArrayResponse<CalendarResponse> getMonthlyCalendar(
             @RequestParam
             @Parameter(description = "조회할 년월", example = "2021-12")
@@ -35,7 +35,7 @@ public interface CalendarApi {
             String familyId
     );
 
-    @Operation(summary = "월별 이벤트 조회", description = "월별로 존재하는 이벤트들을 조회합니다. 모든 가족 구성원이 업로드한 여부 등이 이벤트에 속하며, 해당 날짜에 게시글이 있는 경우만 response 에 포함됩니다.\n ⚠️ 월별 캘린더 조회 API와 반환되는 날짜가 다를 수 있습니다.")
+    @Operation(summary = "월별 이벤트 조회", description = "월별로 존재하는 캘린더의 이벤트들을 조회합니다. 모든 가족 구성원이 업로드한 여부 등이 이벤트에 속하며, 해당 날짜에 게시글이 있는 경우만 response 에 포함됩니다.\n\n ⚠️ 월별 캘린더 조회 API와 반환되는 날짜가 다를 수 있습니다.\n\n ⚠️ 더 이상 type=MONTHLY 를 사용하지 않습니다.")
     @GetMapping(value = "/events", params = {"type=MONTHLY"})
     ArrayResponse<DayEventResponse> getMonthlyEvents(
             @RequestParam
