818f1167cd991daa35534eccb01a826ae6f7a258
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2024-02-12 16:01:56 +0900

    [HOTFIX/1.1.6] hotfix: Hotfix v1.1.6 릴리즈  (#163)
    * docs: update README.md (#160)
    
    * fix: fix post content validation logic
    
    * fix: use `@Validated` for parameter validation

--- member/src/main/java/com/oing/restapi/MemberApi.java
+++ member/src/main/java/com/oing/restapi/MemberApi.java
@@ -10,11 +10,12 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "회원 API", description = "회원 관련 API")
 @RestController
-@Valid
+@Validated
 @RequestMapping("/v1/members")
 public interface MemberApi {
     @Operation(summary = "가족 구성원 프로필 조회", description = "가족 구성원 프로필을 조회합니다.", parameters = {
