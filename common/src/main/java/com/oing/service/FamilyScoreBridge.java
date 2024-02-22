9ec4361ded4e142e28f569736f17b298c20ff197
Kwon770 <sckwon770@gmail.com>
2024-02-22 23:29:53 +0900

    refactor: Remove admin api that must not be used from prod

--- common/src/main/java/com/oing/service/FamilyScoreBridge.java
+++ common/src/main/java/com/oing/service/FamilyScoreBridge.java
@@ -3,9 +3,6 @@ package com.oing.service;
 import java.time.LocalDate;
 
 public interface FamilyScoreBridge {
-
-    void setAllFamilyScoresByPostDateBetween(LocalDate startDate, LocalDate endDate);
-
     void updateAllFamilyTopPercentageHistories(LocalDate historyDate);
 
     int calculateFamilyTopPercentage(int rank, int familiesCount);
--- gateway/src/main/java/com/oing/service/FamilyScoreBridgeImpl.java
+++ gateway/src/main/java/com/oing/service/FamilyScoreBridgeImpl.java
@@ -21,29 +21,7 @@ public class FamilyScoreBridgeImpl implements FamilyScoreBridge {
 
     private final FamilyRepository familyRepository;
     private final FamilyTopPercentageHistoryRepository familyTopPercentageHistoryRepository;
-    private final MemberPostService memberPostService;
 
-    @Override
-    @Transactional
-    public void setAllFamilyScoresByPostDateBetween(LocalDate startDate, LocalDate endDate) {
-        List<Family> families = familyRepository.findAll();
-        for (Family family : families) {
-            family.resetScore();
-            family.addScore(calculateFamilyScoreByPostDateBetween(family.getId(), startDate, endDate));
-        }
-    }
-
-    private int calculateFamilyScoreByPostDateBetween(String familyId, LocalDate startDate, LocalDate endDate) {
-        int familyScore = 0;
-
-        List<MemberPost> familyPosts = memberPostService.findAllByFamilyIdAndCreatedAtBetween(familyId, startDate, endDate);
-        familyScore += familyPosts.size() * NEW_POST_SCORE;
-        familyScore += familyPosts.stream().mapToInt(MemberPost::getCommentCnt).sum() * NEW_COMMENT_SCORE;
-        familyScore += familyPosts.stream().mapToInt(MemberPost::getReactionCnt).sum() * NEW_REACTION_SCORE;
-        familyScore += familyPosts.stream().mapToInt(MemberPost::getReactionCnt).sum() * NEW_REAL_EMOJI_SCORE;
-
-        return familyScore;
-    }
 
     @Override
     @Transactional
