f062c5497e592029be3eeb1d40d2c6aaac085c70
Kwon770 <sckwon770@gmail.com>
2024-02-26 00:32:06 +0900

    feat: Add logging in recordAllFamilyTopPercentageHistoriesMonthly

--- family/src/main/java/com/oing/job/FamilyStatisticJob.java
+++ family/src/main/java/com/oing/job/FamilyStatisticJob.java
@@ -2,12 +2,14 @@ package com.oing.job;
 
 import com.oing.service.FamilyScoreBridge;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class FamilyStatisticJob {
@@ -15,10 +17,14 @@ public class FamilyStatisticJob {
     private final FamilyScoreBridge familyScoreBridge;
 
     @Scheduled(cron = "0 0 1 1 * *") // 매월 1일 01시
-    @SchedulerLock(name = "MonthlyFamilyTopPercentageHistoryRecordingScheduling", lockAtMostFor = "PT30S", lockAtLeastFor = "PT30S")
+    @SchedulerLock(name = "MonthlyFamilyTopPercentageHistoryRecordingSchedule", lockAtMostFor = "PT30S", lockAtLeastFor = "PT30S")
     public void recordAllFamilyTopPercentageHistoriesMonthly() {
         // 방금 막 지나간 지난 달의 데이터를 기록한다.
         LocalDate historyDate = LocalDate.now().minusMonths(1);
-        familyScoreBridge.updateAllFamilyTopPercentageHistories(historyDate.getYear(), historyDate.getMonthValue());
+        int historyYear = historyDate.getYear();
+        int historyMonth = historyDate.getMonthValue();
+        log.info("[MonthlyFamilyTopPercentageHistoryRecordingSchedule: {}-{}] scheduled and locked for 30s", historyYear, historyMonth);
+
+        familyScoreBridge.updateAllFamilyTopPercentageHistories(historyYear, historyMonth);
     }
 }
