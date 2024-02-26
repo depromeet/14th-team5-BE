b9b4fc2b41f7b5aec86c999951b5b8fa92842ed2
Kwon770 <sckwon770@gmail.com>
2024-02-26 13:21:01 +0900

    fix: Fix broken flyway initlization sql file

--- gateway/src/main/resources/db/migration/V202402252319__alter_FamilyTopPercentageHistory_tbl.sql
+++ gateway/src/main/resources/db/migration/V202402252319__alter_FamilyTopPercentageHistory_tbl.sql
@@ -1,4 +1,7 @@
 ALTER TABLE family_top_percentage_history
+DROP PRIMARY KEY;
+
+ALTER TABLE family_top_percentage_history
 DROP COLUMN date;
 
 ALTER TABLE family_top_percentage_history
@@ -7,4 +10,5 @@ ADD COLUMN history_year INT NOT NULL DEFAULT 2024;
 ALTER TABLE family_top_percentage_history
 ADD COLUMN history_month INT NOT NULL DEFAULT 1;
 
-
+ALTER TABLE family_top_percentage_history
+ADD PRIMARY KEY (family_id, history_year, history_month);
