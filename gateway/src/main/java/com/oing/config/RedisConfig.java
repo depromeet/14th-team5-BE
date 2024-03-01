12f2b54c0a3937aef7155033148cd32811c127c5
송영민 (YeongMin Song) <jakgon@naver.com>
2024-03-01 22:50:05 +0900

    [OING-215] feat: AWS 프로덕션용 설정 일부 변경 (#191)
    * feat: update redis config
    
    * feat: update container spec
    
    * feat: update memory usage
    
    * feat: update log pattern production

--- gateway/src/main/java/com/oing/config/RedisConfig.java
+++ gateway/src/main/java/com/oing/config/RedisConfig.java
@@ -3,12 +3,15 @@ package com.oing.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.Collections;
 
 @Configuration
 public class RedisConfig {
@@ -21,7 +24,8 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+        LettuceClientConfiguration configuration = LettuceClientConfiguration.builder().useSsl().build();
+        return new LettuceConnectionFactory(new RedisClusterConfiguration(Collections.singleton(redisHost + ":" + redisPort)), configuration);
     }
 
     @Bean
