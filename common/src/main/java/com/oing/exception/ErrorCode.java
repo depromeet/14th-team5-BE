72520c89ec228e1230ee77377e05adbc4f5ea80b
Kwon770 <sckwon770@gmail.com>
2024-04-05 00:36:54 +0900

    refactor: Translate english error message to Korean

--- common/src/main/java/com/oing/exception/ErrorCode.java
+++ common/src/main/java/com/oing/exception/ErrorCode.java
@@ -18,51 +18,53 @@ public enum ErrorCode {
      */
     APP_UPDATE_REQUIRED("AV0001", "Update Required"),
     APP_KEY_NOT_FOUND("AV0002", "App Key Not Found"),
+
     /**
      * Auth Related Errors
      */
-    AUTHENTICATION_FAILED("AU0001", "Authentication failed"),
-    TOKEN_AUTHENTICATION_FAILED("AU0002", "Token Authentication failed"),
-    AUTHORIZATION_FAILED("AU0003", "No Permission"),
-    REFRESH_TOKEN_INVALID("AU0004", "Refresh Token is invalid"),
-    TOKEN_EXPIRED("AU0005", "Token is expired"),
+    AUTHENTICATION_FAILED("AU0001", "인증에 실패했습니다."),
+    TOKEN_AUTHENTICATION_FAILED("AU0002", "토큰을 통한 인증에 실패했습니다."),
+    AUTHORIZATION_FAILED("AU0003", "권한이 없습니다."),
+    REFRESH_TOKEN_INVALID("AU0004", "유효하지 않은 리프레쉬 토큰입니다."),
+    TOKEN_EXPIRED("AU0005", "토큰이 만료됐습니다."),
+
     /**
      * Member Related Errors
      */
-    MEMBER_NOT_FOUND("MB0001", "Member not found"),
-    MEMBER_ALREADY_EXISTS("MB0002", "Member With That Credentials Already Exists"),
+    MEMBER_NOT_FOUND("MB0001", "찾을 수 없는 회원입니다."),
+    MEMBER_ALREADY_EXISTS("MB0002", "이미 존재하는 회원입니다."),
+
     /**
      * Post Related Errors
      */
-    POST_NOT_FOUND("PO0001", "Post not found"),
+    POST_NOT_FOUND("PO0001", "존재하지 않는 게시글입니다."),
+    INVALID_UPLOAD_TIME("PO0002", "게시글 업로드가 허용되지 않은 시간대입니다."),
+    DUPLICATE_POST_UPLOAD("PO0003", "이미 게시글을 업로드했습니다."),
 
     /**
      * Emoji Related Errors
      */
-    EMOJI_ALREADY_EXISTS("EM0001", "Emoji already exists"),
-    EMOJI_NOT_FOUND("EM0002", "Emoji not found"),
+    EMOJI_ALREADY_EXISTS("EM0001", "이미 존재하는 이모지입니다."),
+    EMOJI_NOT_FOUND("EM0002", "존재하지 않는 이모지입니다."),
+
     /**
      * Comment Related Errors
      */
-    POST_COMMENT_NOT_FOUND("CM0001", "Comment not found"),
+    POST_COMMENT_NOT_FOUND("CM0001", "존재하지 않는 댓글입니다."),
+
     /**
      * Family Related Errors
      */
-    FAMILY_NOT_FOUND("FM0001", "Family not found"),
-    ALREADY_IN_FAMILY("FM0002", "Already in family"),
-    /**
-     * Post Related Errors
-     */
-    INVALID_UPLOAD_TIME("PO0001", "Invalid Upload Time. The request is outside the valid time range" +
-            "(from 12:00 AM yesterday to 12:00 AM today)."),
-    DUPLICATE_POST_UPLOAD("PO0002", "Duplicate Post Upload"),
+    FAMILY_NOT_FOUND("FM0001", "존재하지 않는 가족입니다."),
+    ALREADY_IN_FAMILY("FM0002", "이미 가족에 가입되어 있습니다."),
+
     /**
      * Real-Emoji Related Errors
      */
-    REAL_EMOJI_NOT_FOUND("RE0001", "Real-Emoji not found"),
-    REAL_EMOJI_ALREADY_EXISTS("RE0002", "Real-Emoji already exists"),
-    REGISTERED_REAL_EMOJI_NOT_FOUND("RE0003", "Registered Real-Emoji not found"),
-    DUPLICATE_REAL_EMOJI("RE0004", "Duplicate Real Emoji"),
+    REAL_EMOJI_NOT_FOUND("RE0001", "존재하지 않는 리얼이모지입니다."),
+    REAL_EMOJI_ALREADY_EXISTS("RE0002", "이미 존재하는 리얼이모지입니다."),
+    REGISTERED_REAL_EMOJI_NOT_FOUND("RE0003", "등록된 리얼이모지가 아닙니다."),
+    DUPLICATE_REAL_EMOJI("RE0004", "이미 등록된 리얼이모지입니다."),
     /**
      * Deep Link Related Errors
      */
