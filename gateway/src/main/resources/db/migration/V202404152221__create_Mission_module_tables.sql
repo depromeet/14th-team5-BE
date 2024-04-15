6a8559539605ed35ea5ec4127887f5483f8d66ec
Kwon770 <sckwon770@gmail.com>
2024-04-15 22:35:42 +0900

    feat: Add Mission, DailyMissionHistory entity and db.migration flyway file

---
+++ gateway/src/main/resources/db/migration/V202404152221__create_Mission_module_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE 'mission'
+(
+    'created_at' DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    'updated_at' DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    'content'    VARCHAR(255) NOT NULL,
+    'mission_id' CHAR(26)     NOT NULL COMMENT 'UUID',
+    PRIMARY KEY ('mission_id')
+) DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci comment '미션테이블';
+
+CREATE TABLE 'daily_mission_history'
+(
+    'date'       DATE         NOT NULL,
+    'created_at' DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    'mission_id' CHAR(26)     NOT NULL,
+    FOREIGN KEY 'daily_mission_history_fk1' ('mission_id') REFERENCES 'mission' ('mission_id'),
+    PRIMARY KEY ('date')
+) DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci comment '일일미션내역테이블';
---
+++ mission/src/main/java/com/oing/domain/Mission.java
@@ -0,0 +1,21 @@
+package com.oing.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity(name = "mission")
+public class Mission extends BaseAuditEntity {
+    @Id
+    @Column(name = "mission_id", columnDefinition = "CHAR(26)", nullable = false)
+    private String id;
+
+    @Column(nullable = false)
+    private String content;
+}
