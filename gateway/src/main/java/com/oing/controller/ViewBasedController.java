9d98b97e5d7dbbd1bd45fae96157e90cb8c85ccd
송영민 (YeongMin Song) <jakgon@naver.com>
2024-04-20 19:31:47 +0900

    [OING-311] feat: 뷰 기반 API 중 메인 페이지 API 구현 (#237)
    * feat: add view based api
    
    * docs: update view based api docs
    
    * feat: add isMeUploaded
    
    * fix: update birthday evaluate logics

---
+++ gateway/src/main/java/com/oing/controller/ViewBasedController.java
@@ -0,0 +1,117 @@
+package com.oing.controller;
+
+import com.oing.domain.*;
+import com.oing.dto.response.*;
+import com.oing.restapi.ViewBasedApi;
+import com.oing.service.MemberBridge;
+import com.oing.service.MemberPickService;
+import com.oing.service.MemberService;
+import com.oing.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
+
+/**
+ * no5ing-server
+ * User: CChuYong
+ * Date: 4/16/24
+ * Time: 4:03 PM
+ */
+@Controller
+@RequiredArgsConstructor
+public class ViewBasedController implements ViewBasedApi {
+    private final PostService postService;
+    private final MemberService memberService;
+    private final MemberPickService memberPickService;
+    private final MemberBridge memberBridge;
+
+    private static final int PAGE_FETCH_SIZE = 1000;
+    @Override
+    public MainPageResponse getMainPage(
+            String loginMemberId
+    ) {
+        String familyId = memberBridge.getFamilyIdByMemberId(loginMemberId);
+        LocalDate today = LocalDate.now();
+        Map<String, FamilyMemberProfileResponse> memberMap = new HashMap<>();
+        Page<FamilyMemberProfileResponse> members = memberService.getFamilyMembersProfilesByFamilyId(familyId, 1, PAGE_FETCH_SIZE);
+        members.forEach(member -> memberMap.put(member.memberId(), member));
+        Collection<Post> survivalPosts = postService.searchMemberPost(
+                1, PAGE_FETCH_SIZE, today, null, loginMemberId, familyId,
+                true, PostType.SURVIVAL
+        ).results();
+        Collection<Post> missionPosts = postService.searchMemberPost(
+                1, PAGE_FETCH_SIZE, today, null, loginMemberId, familyId,
+                true, PostType.MISSION
+        ).results();
+        Map<String, Integer> postUploaderRankMap = new HashMap<>();
+        int lastIdx = 1;
+        for (Post survivalPost : survivalPosts) {
+            postUploaderRankMap.put(survivalPost.getMemberId(), lastIdx++);
+        }
+        Comparator<FamilyMemberProfileResponse> comparator = Comparator
+                .comparing((FamilyMemberProfileResponse member) -> !member.memberId().equals(loginMemberId))
+                .thenComparing((FamilyMemberProfileResponse member) -> {
+                    Integer rank = postUploaderRankMap.get(member.memberId());
+                    return rank != null ? rank : Integer.MAX_VALUE;
+                });
+        Set<String> pickedSet = new HashSet<>();
+        memberPickService.getPickedMembers(familyId, loginMemberId)
+                .forEach(pick -> pickedSet.add(pick.getToMemberId()));
+
+
+        return new MainPageResponse(
+                members.stream().sorted(comparator).map((member) -> new MainPageTopBarResponse(
+                        member.memberId(),
+                        member.imageUrl(),
+                        String.valueOf(member.name().charAt(0)),
+                        member.name(),
+                        postUploaderRankMap.get(member.memberId()),
+                        member.dayOfBirth().getMonth() == today.getMonth()
+                                && member.dayOfBirth().getDayOfMonth() == today.getDayOfMonth(),
+                        !pickedSet.contains(member.memberId())
+                                && !member.memberId().equals(loginMemberId)
+                        && !postUploaderRankMap.containsKey(member.memberId())
+                )).toList(),
+                true,
+                postUploaderRankMap.containsKey(loginMemberId),
+                survivalPosts.stream().map(post -> {
+                    FamilyMemberProfileResponse member = memberMap.get(post.getMemberId());
+                    return new MainPageFeedResponse(
+                            post.getId(),
+                            post.getPostImgUrl(),
+                            member != null ? member.name() : "UNKNOWN",
+                            post.getCreatedAt().atZone(ZoneId.systemDefault())
+                    );
+                }).toList(),
+                missionPosts.stream().map(post -> {
+                    FamilyMemberProfileResponse member = memberMap.get(post.getMemberId());
+                    return new MainPageFeedResponse(
+                            post.getId(),
+                            post.getPostImgUrl(),
+                            member != null ? member.name() : "UNKNOWN",
+                            post.getCreatedAt().atZone(ZoneId.systemDefault())
+                    );
+                }).toList(),
+                memberPickService.getPickMembers(familyId, loginMemberId).stream().map(pickMember -> {
+                    FamilyMemberProfileResponse member = memberMap.get(pickMember.getFromMemberId());
+                    if(member == null) {
+                        return new MainPagePickerResponse(
+                                pickMember.getFromMemberId(),
+                                "UNKNOWN",
+                                "UNKNOWN"
+                        );
+                    }
+                    return new MainPagePickerResponse(
+                            member.memberId(),
+                            member.imageUrl(),
+                            member.name()
+                    );
+                }).toList()
+
+        );
+    }
+}
---
+++ gateway/src/main/java/com/oing/dto/response/MainPageTopBarResponse.java
@@ -0,0 +1,34 @@
+package com.oing.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * no5ing-server
+ * User: CChuYong
+ * Date: 4/16/24
+ * Time: 4:07 PM
+ */
+@Schema(description = "메인 페이지 상단 바 요소")
+public record MainPageTopBarResponse(
+        @Schema(description = "멤버ID", example = "01HGW2N7EHJVJ4CJ999RRS2E97")
+        String memberId,
+
+        @Schema(description = "이미지 URL", example = "https://no5ing.com/image/01HGW2N7EHJVJ4CJ999RRS2E97")
+        String imageUrl,
+
+        @Schema(description = "이미지 없을 때 표시할 글자", example = "엄")
+        String noImageLetter,
+
+        @Schema(description = "닉네임", example = "엄마")
+        String displayName,
+
+        @Schema(description = "순위 표기", example = "1")
+        Integer displayRank,
+
+        @Schema(description = "생일 마크 표시 여부", example = "true")
+        boolean shouldShowBirthdayMark,
+
+        @Schema(description = "찌르기 아이콘 표시 여부", example = "true")
+        boolean shouldShowPickIcon
+) {
+}
