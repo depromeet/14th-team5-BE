042c99014bbc299cd9324a2453001be7ebb64375
Kwon770 <sckwon770@gmail.com>
2024-04-25 00:25:00 +0900

    feat: Add mostRecentSurvivalPostDate in FamilyMemberMonthlyRankingResponse according to requirement

--- gateway/src/main/java/com/oing/controller/MainViewController.java
+++ gateway/src/main/java/com/oing/controller/MainViewController.java
@@ -165,7 +165,8 @@ public class MainViewController implements MainViewApi {
         FamilyMemberRankerResponse first = new FamilyMemberRankerResponse("https://static01.nyt.com/images/2016/09/28/us/28xp-pepefrog/28xp-pepefrog-superJumbo.jpg", "정신적 지주", 24);
         FamilyMemberRankerResponse second = new FamilyMemberRankerResponse("https://static01.nyt.com/images/2016/09/28/us/28xp-pepefrog/28xp-pepefrog-superJumbo.jpg", "권순찬", 23);
         FamilyMemberRankerResponse third = null;
+        LocalDate mostRecentSurvivalPostDate = LocalDate.now().minusDays(1);
 
-        return new FamilyMemberMonthlyRankingResponse(4, first, second, third);
+        return new FamilyMemberMonthlyRankingResponse(4, first, second, third, mostRecentSurvivalPostDate);
     }
 }
--- gateway/src/main/java/com/oing/dto/response/FamilyMemberMonthlyRankingResponse.java
+++ gateway/src/main/java/com/oing/dto/response/FamilyMemberMonthlyRankingResponse.java
@@ -1,6 +1,10 @@
 package com.oing.dto.response;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Schema(description = "가족구성원 월간 랭킹 응답")
 public record FamilyMemberMonthlyRankingResponse(
@@ -15,6 +19,10 @@ public record FamilyMemberMonthlyRankingResponse(
         FamilyMemberRankerResponse secondRanker,
 
         @Schema(description = "(응답모킹됨) 3등 랭커 Dto")
-        FamilyMemberRankerResponse thirdRanker
+        FamilyMemberRankerResponse thirdRanker,
+
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        @Parameter(description = "가장 최근에 생존신고한 날", example = "2023-12-05")
+        LocalDate mostRecentSurvivalPostDate
 ) {
 }
