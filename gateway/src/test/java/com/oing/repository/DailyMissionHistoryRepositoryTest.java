69978d6203056718d6439c470b026e562e1ea8ec
Kwon770 <sckwon770@gmail.com>
2024-05-07 23:56:49 +0900

    fix: Make Nested test class lower case

--- gateway/src/test/java/com/oing/repository/DailyMissionHistoryRepositoryTest.java
+++ gateway/src/test/java/com/oing/repository/DailyMissionHistoryRepositoryTest.java
@@ -77,7 +77,7 @@ public class DailyMissionHistoryRepositoryTest {
 
 
     @Nested
-    class FindRecentDailyMissionIdsOrderByDateAsc {
+    class findRecentDailyMissionIdsOrderByDateAsc {
         @Test
         void 정상_조회_테스트() {
             // given
