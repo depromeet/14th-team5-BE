5d6ff7564985aa42a016f2f9557d88527ed26fca
jisu <dlawotn3@naver.com>
2024-06-18 19:30:26 +0900

    chore: add delete previous data from deep_link and family_invite_link tbl

--- gateway/src/main/resources/db/migration/V202406171820__add_column_to_deep_link_and_family_invite_link_tbl.sql
+++ gateway/src/main/resources/db/migration/V202406171820__add_column_to_deep_link_and_family_invite_link_tbl.sql
@@ -1,3 +1,8 @@
-ALTER TABLE `deep_link` ADD COLUMN (`member_id` CHAR(26) NOT NULL COMMENT 'ULID');
-ALTER TABLE `deep_link` ADD COLUMN (`family_id` CHAR(26) NOT NULL COMMENT 'ULID');
-ALTER TABLE `family_invite_link` ADD COLUMN (`member_id` CHAR(26) NOT NULL COMMENT 'ULID');
+ALTER TABLE `deep_link` ADD COLUMN (`member_id` CHAR(26) COMMENT 'ULID');
+ALTER TABLE `deep_link` ADD COLUMN (`family_id` CHAR(26) COMMENT 'ULID');
+ALTER TABLE `family_invite_link` ADD COLUMN (`member_id` CHAR(26) COMMENT 'ULID');
+DELETE FROM `deep_link` WHERE `family_id` IS NULL OR `member_id` IS NULL;
+DELETE FROM `family_invite_link` WHERE `member_id` IS NULL;
+ALTER TABLE `deep_link` MODIFY COLUMN `member_id` CHAR(26) NOT NULL;
+ALTER TABLE `deep_link` MODIFY COLUMN `family_id` CHAR(26) NOT NULL;
+ALTER TABLE `family_invite_link` MODIFY COLUMN `member_id` CHAR(26) NOT NULL;
