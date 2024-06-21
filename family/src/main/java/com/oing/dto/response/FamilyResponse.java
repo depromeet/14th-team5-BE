b873c34d9bf48307f1f24c66ccbbe3f9c2ab0a86
jisu <dlawotn3@naver.com>
2024-06-21 15:10:04 +0900

    feature: add familyName field in api

--- family/src/main/java/com/oing/dto/response/FamilyResponse.java
+++ family/src/main/java/com/oing/dto/response/FamilyResponse.java
@@ -11,12 +11,16 @@ public record FamilyResponse(
         @Schema(description = "가족 ID", example = "01HGW2N7EHJVJ4CJ999RRS2E97")
         String familyId,
 
+        @Schema(description = "가족 이름", example = "미밍이네")
+        String familyName,
+
         @Schema(description = "가족 생성 시간", example = "2021-12-05T12:30:00.000+09:00")
         ZonedDateTime createdAt
 ) {
     public static FamilyResponse of(Family family) {
         return new FamilyResponse(
                 family.getId(),
+                family.getFamilyName(),
                 family.getCreatedAt().atZone(ZoneId.systemDefault())
         );
     }
