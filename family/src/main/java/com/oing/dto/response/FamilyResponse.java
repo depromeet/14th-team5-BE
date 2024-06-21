345fe021fabbfab362157df5e7f6cd4da70d8990
jisu <dlawotn3@naver.com>
2024-06-21 16:03:31 +0900

    refactor: add familyNameEditorId to FamilyResponse

--- family/src/main/java/com/oing/dto/response/FamilyResponse.java
+++ family/src/main/java/com/oing/dto/response/FamilyResponse.java
@@ -14,6 +14,9 @@ public record FamilyResponse(
         @Schema(description = "가족 이름", example = "미밍이네")
         String familyName,
 
+        @Schema(description = "가족 이름 마지막 수정자 ID", example = "01HGW2N7EHJVJ4CJ999RRS2E97")
+        String familyNameEditorId,
+
         @Schema(description = "가족 생성 시간", example = "2021-12-05T12:30:00.000+09:00")
         ZonedDateTime createdAt
 ) {
@@ -21,6 +24,7 @@ public record FamilyResponse(
         return new FamilyResponse(
                 family.getId(),
                 family.getFamilyName(),
+                family.getFamilyNameEditorId(),
                 family.getCreatedAt().atZone(ZoneId.systemDefault())
         );
     }
