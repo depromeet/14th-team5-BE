4b0fe80c302deddefebd2f91b37496f4e37e1592
ChuYong <jakgon@naver.com>
2024-12-05 19:53:41 +0900

    feat: add notifications style, add deeplink

--- gateway/src/main/java/com/oing/dto/response/NotificationResponse.java
+++ gateway/src/main/java/com/oing/dto/response/NotificationResponse.java
@@ -1,5 +1,6 @@
 package com.oing.dto.response;
 
+import com.oing.domain.NotificationStyle;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.ZonedDateTime;
@@ -12,11 +13,20 @@ public record NotificationResponse(
         @Schema(description = "발송자 이미지 url", example = "https://..")
         String senderImageUrl,
 
+        @Schema(description = "알림 스타일", example = "BIRTHDAY")
+        NotificationStyle style,
+
         @Schema(description = "알림 제목", example = "우리 가족 모두가 생존신고를 완료했어요")
         String title,
 
         @Schema(description = "알림 내용", example = "우리 가족 모두가 생존신고를 완료했어요")
         String content,
+
+        @Schema(description = "ios 기준 딥링크", example = "https://..")
+        String iosDeepLink,
+
+        @Schema(description = "안드로이드 기준 딥링크", example = "https://..")
+        String aosDeepLink,
 
         @Schema(description = "알림 생성 시간", example = "2023-12-23T01:53:21.577347+09:00")
         ZonedDateTime createdAt
