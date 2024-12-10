59020ed4b343e2f03018d2b20782ec260a811068
jisu <dlawotn3@naver.com>
2024-12-10 17:56:19 +0900

    style: delete mock api code in VoiceComment

--- post/src/main/java/com/oing/restapi/VoiceCommentApi.java
+++ post/src/main/java/com/oing/restapi/VoiceCommentApi.java
@@ -3,7 +3,6 @@ package com.oing.restapi;
 import com.oing.dto.request.CreatePostCommentRequest;
 import com.oing.dto.request.PreSignedUrlRequest;
 import com.oing.dto.response.DefaultResponse;
-import com.oing.dto.response.PaginationResponse;
 import com.oing.dto.response.PostCommentResponseV2;
 import com.oing.dto.response.PreSignedUrlResponse;
 import com.oing.util.security.LoginMemberId;
@@ -11,7 +10,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "게시물 음성 댓글 API", description = "게시물 음성 댓글 관련 API")
@@ -57,32 +55,6 @@ public interface VoiceCommentApi {
             @Parameter(description = "음성 댓글 ID", example = "01HGW2N7EHJVJ4CJ999RRS2E97")
             @PathVariable
             String commentId,
-
-            @Parameter(hidden = true)
-            @LoginMemberId
-            String loginMemberId
-    );
-
-    @Operation(summary = "게시물 음성 댓글 조회", description = "게시물에 달린 음성 댓글을 조회합니다.")
-    @GetMapping
-    PaginationResponse<PostCommentResponseV2> getPostComments(
-            @Parameter(description = "게시물 ID", example = "01HGW2N7EHJVJ4CJ999RRS2E97")
-            @PathVariable
-            String postId,
-
-            @RequestParam(required = false, defaultValue = "1")
-            @Parameter(description = "가져올 현재 페이지", example = "1")
-            @Min(value = 1)
-            Integer page,
-
-            @RequestParam(required = false, defaultValue = "10")
-            @Parameter(description = "가져올 페이지당 크기", example = "10")
-            @Min(value = 1)
-            Integer size,
-
-            @RequestParam(required = false)
-            @Parameter(description = "정렬 방식", example = "DESC | ASC")
-            String sort,
 
             @Parameter(hidden = true)
             @LoginMemberId
