bc22e39a9b072a5676c5e27108435ff0a9d6aa63
jisu <dlawotn3@naver.com>
2024-12-10 17:56:19 +0900

    style: delete mock api code in VoiceComment

--- post/src/main/java/com/oing/controller/VoiceCommentController.java
+++ post/src/main/java/com/oing/controller/VoiceCommentController.java
@@ -6,7 +6,6 @@ import com.oing.domain.VoiceComment;
 import com.oing.dto.request.CreatePostCommentRequest;
 import com.oing.dto.request.PreSignedUrlRequest;
 import com.oing.dto.response.DefaultResponse;
-import com.oing.dto.response.PaginationResponse;
 import com.oing.dto.response.PostCommentResponseV2;
 import com.oing.dto.response.PreSignedUrlResponse;
 import com.oing.restapi.VoiceCommentApi;
@@ -18,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
 import java.time.ZonedDateTime;
-import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -63,27 +61,5 @@ public class VoiceCommentController implements VoiceCommentApi {
         voiceCommentService.deleteVoiceComment(post, commentId, loginMemberId);
         log.info("Member {} has deleted voice-comment {}", loginMemberId, commentId);
         return DefaultResponse.ok();
-    }
-
-    @Override
-    public PaginationResponse<PostCommentResponseV2> getPostComments(String postId, Integer page, Integer size,
-                                                                     String sort, String loginMemberId) {
-        return new PaginationResponse<>(
-                1,
-                1,
-                size,
-                false,
-                List.of(
-                        new PostCommentResponseV2(
-                                "01HGW2N7EHJVJ4CJ999RRS2E97",
-                                CommentType.VOICE,
-                                postId,
-                                loginMemberId,
-                                "앱 버전 업데이트 후에 확인할 수 있는 댓글이에요",
-                                "https://..",
-                                ZonedDateTime.now()
-                        )
-                )
-        );
     }
 }
