b62428b180274e6839117553fe2a18da96db1f7e
Kwon770 <sckwon770@gmail.com>
2024-12-24 22:24:51 +0900

    feat: Add the trigger to create userNotificationHistory on NewComment and MissionUnlocked

--- gateway/src/main/java/com/oing/service/UserNotificationHistoryService.java
+++ gateway/src/main/java/com/oing/service/UserNotificationHistoryService.java
@@ -23,7 +23,33 @@ public class UserNotificationHistoryService {
     private final IdentityGenerator identityGenerator;
 
     @Transactional
-    public UserNotificationHistory createUserNotificationHistory(CreateUserNotificationHistoryDTO createUserNotificationHistoryDTO) {
+    public void appendMissionUnlockedNotiHistory(List<String> receiverFamilyMemberIds) {
+        receiverFamilyMemberIds.forEach(receiverFamilyMemberId -> createUserNotificationHistory(
+                new CreateUserNotificationHistoryDTO(
+                        "우리 가족 모두가 생존신고를 완료했어요!",
+                        "이제 미션 사진을 업로드할 수 있어요",
+                        null, // TODO : 메인화면 딥링크 주소 필요
+                        null,
+                        "99999999999999999999999999",
+                        receiverFamilyMemberId
+                ))
+        );
+    }
+
+    @Transactional
+    public void appendCommentNotiHistory(String senderMemberName, String comment, String senderMemberId, String receiverMemberId, String aosDeepLink, String iosDeepLink) {
+        createUserNotificationHistory(
+                new CreateUserNotificationHistoryDTO(
+                        String.format("%s님이 내 생존신고에 댓글을 달았어요", senderMemberName),
+                        comment,
+                        aosDeepLink,
+                        iosDeepLink,
+                        senderMemberId,
+                        receiverMemberId
+                ));
+    }
+
+    private UserNotificationHistory createUserNotificationHistory(CreateUserNotificationHistoryDTO createUserNotificationHistoryDTO) {
         return userNotificationHistoryRepository.save(
                 createUserNotificationHistoryDTO.toEntity(identityGenerator.generateIdentity()));
     }
