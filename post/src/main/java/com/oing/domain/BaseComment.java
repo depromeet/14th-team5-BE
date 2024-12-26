6f0a5bd97ea70045874f1a82e1f3c23f5b78b68a
jisu <dlawotn3@naver.com>
2024-12-26 13:59:55 +0900

    feat: add BaseComment

---
+++ post/src/main/java/com/oing/domain/BaseComment.java
@@ -0,0 +1,21 @@
+package com.oing.domain;
+
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.Comparator;
+
+@MappedSuperclass
+public abstract class BaseComment extends BaseAuditEntity {
+    public abstract String getContent();
+    public abstract String getMemberId();
+    public abstract String getId();
+    public abstract String getPostId();
+
+    public static Comparator<BaseComment> getComparator(String sort) {
+        if (sort == null || sort.equalsIgnoreCase("ASC")) {
+            return Comparator.comparing(BaseComment::getCreatedAt);
+        } else {
+            return Comparator.comparing(BaseComment::getCreatedAt).reversed();
+        }
+    }
+}
