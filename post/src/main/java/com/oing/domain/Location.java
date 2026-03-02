f4c8cebc1ae62e32246e2710adefaa789e7ec24f
Jisu Lim <dlawotn3@naver.com>
2026-03-02 19:57:39 +0900

    feat: add Location field to Post

---
+++ post/src/main/java/com/oing/domain/Location.java
@@ -0,0 +1,24 @@
+package com.oing.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Location {
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "address")
+    private String address;
+}
