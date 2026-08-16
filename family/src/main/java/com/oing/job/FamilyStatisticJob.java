1af7561a79ac856805e9e29da42e17f8c8995c09
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2026-08-16 16:16:16 +0900

    Remove unnecessary blank line in FamilyStatisticJob (#323)

--- family/src/main/java/com/oing/job/FamilyStatisticJob.java
+++ family/src/main/java/com/oing/job/FamilyStatisticJob.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDate;
 
 @Slf4j
